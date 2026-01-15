ce3301a1b66dfeb94e09149aac286e32efdc977f
Jake Soenneker <jake@soeneker.com>
2026-01-15 13:18:50 -0600

    Move to MTP

---
+++ src/ProcessCaptureMode.cs
@@ -0,0 +1,11 @@
+using Intellenum;
+
+namespace Soenneker.Enums.ProcessCaptureModes;
+
+/// <summary>
+/// Defines how process output should be captured.
+/// </summary>
+[Intellenum<string>]
+public partial class ProcessCaptureMode
+{
+}
---
+++ test/Soenneker.Enums.ProcessCaptureModes.Tests/ProcessCaptureModeTests.cs
@@ -0,0 +1,13 @@
+using Soenneker.Tests.Unit;
+using Xunit;
+
+namespace Soenneker.Enums.ProcessCaptureModes.Tests;
+
+public sealed class ProcessCaptureModeTests : UnitTest
+{
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
