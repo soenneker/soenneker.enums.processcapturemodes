b259ab2df2095b5380c2dc5e3e9669b5fd1f485f
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:01:47 -0500

    TUnit conversion

--- test/Soenneker.Enums.ProcessCaptureModes.Tests/ProcessCaptureModeTests.cs
+++ test/Soenneker.Enums.ProcessCaptureModes.Tests/ProcessCaptureModeTests.cs
@@ -1,11 +1,10 @@
 using Soenneker.Tests.Unit;
-using Xunit;
 
 namespace Soenneker.Enums.ProcessCaptureModes.Tests;
 
 public sealed class ProcessCaptureModeTests : UnitTest
 {
-    [Fact]
+    [Test]
     public void Default()
     {
 
